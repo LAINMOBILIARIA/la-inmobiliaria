9c2c73da6345b7a63f62a2e1a71da02b155ab541
Lucas Teixeira <admlainmuebles@gmail.com>
2025-07-07 17:41:37 -0600

    Novo site com layout profissional e navegação

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
 
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
 
-function Home() {
+export default function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>L&A INMOBILIARIA</h1>
-      <nav style={{ marginTop: 20 }}>
-        <ul style={{ listStyle: "none", padding: 0 }}>
-          <li><Link to="/casas-venta">Casas en Venta</Link></li>
-          <li><Link to="/casas-renta">Casas en Renta</Link></li>
-          <li><Link to="/departamentos-venta">Departamentos en Venta</Link></li>
-          <li><Link to="/departamentos-renta">Departamentos en Renta</Link></li>
-        </ul>
-      </nav>
+    <div className="container">
+      <header>
+        <h1>L&A INMOBILIARIA</h1>
+        <nav>
+          <ul>
+            <li><Link to="/casas-venta">Casas Venta</Link></li>
+            <li><Link to="/casas-renta">Casas Renta</Link></li>
+            <li><Link to="/departamentos-venta">Departamentos Venta</Link></li>
+            <li><Link to="/departamentos-renta">Departamentos Renta</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
     </div>
   );
 }
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  );
-}
-
-export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Bienvenidos a L&A INMOBILIARIA</h2>
+      <p>Seleccione una categoría en el menú superior para ver nuestras propiedades.</p>
+    </div>
+  );
+}
